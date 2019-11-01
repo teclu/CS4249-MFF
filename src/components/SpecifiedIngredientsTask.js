511d91a52a316b81ef969acf0aa3afa8c3763387
Stanley Tay <saintpatricktay@gmail.com>
2019-11-01 16:41:30 +0800

    chore: adjust task CSS

--- src/components/SpecifiedIngredientsTask.js
+++ src/components/SpecifiedIngredientsTask.js
@@ -2,6 +2,7 @@ import React from 'react';
 import queryString from 'query-string'
 import Box from '@material-ui/core/Box';
 import SpecifiedIngredientsTasks from '../SpecifiedIngredientsTasks.js'
+import '../css/SpecifiedIngredientsTasks.css';
 
 /*
  * This records the background DVs we want to measure and also acts as a sender to the Google Forms.
@@ -12,24 +13,20 @@ class SpecifiedIngredientsTask extends React.Component {
     render() {
         const queryStringParameters = queryString.parse(this.props.location.search);
         const specifiedIngredients = SpecifiedIngredientsTasks[queryStringParameters.Categories];
-        let instructionToRender = "Select the following ingredients: ";
+        let instructionToRender = [];
+        instructionToRender.push(<p>Select the following ingredients (the order does not matter):</p>)
 
         // Randomise the Ingredients' order.
         specifiedIngredients.sort(() => Math.random() - 0.5);
 
         // Append the Ingredients to the instruction string.
         for (const specifiedIngredient of specifiedIngredients) {
-            instructionToRender = instructionToRender.concat(specifiedIngredient);
-            if (specifiedIngredients.indexOf(specifiedIngredient) !== specifiedIngredients.length - 1) {
-                instructionToRender = instructionToRender.concat(', ');
-            }
-            else {
-                instructionToRender = instructionToRender.concat('.');
-            }
+            instructionToRender.push(<li className="SpecifiedIngredient">{specifiedIngredient}</li>);
         }
 
         return (
-            <Box>
+            <Box className="InstructionToRender">
+                <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet"></link>
                 {instructionToRender}
             </Box>
         );
